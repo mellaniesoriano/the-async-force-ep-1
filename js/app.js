07e81dd9c724a106bfe3630380143ee0e785bb7a
mellaniesoriano <msoriano808@gmail.com>
2017-06-08 12:32:52 -1000

    created dynamic function to get person data

--- js/app.js
+++ js/app.js
@@ -1,61 +1,66 @@
-
 (function(){
 
-  function getElements(id, objVal) {
-    var getElement = document.getElementById(id);
-    getElement.innerHTML = objVal;
-  }
-
-  function darthVader(func) {
-    var response = JSON.parse(this.responseText);
-    console.log(response);
-
-    getElements('person4Name', response.name);
-    getElements('person4HomeWorld', response.species);
-  }
-
-  function hanSolo(func) {
-    var response = JSON.parse(this.responseText);
-    console.log(response);
-
-    getElements('person14Name', response.name);
-    getElements('person14Species', response.species);
-  }
-
-  function getFilmList(func) {
-    var response = JSON.parse(this.responseText);
-    var displayFilmList = document.getElementById('filmList');
-    console.log(response.results);
-
-    function makeList(type, className, content, appendTarget) {
-      var newElement = document.createElement(type);
-      newElement.className = className;
-      newElement.innerHTML = content;
-      appendTarget.appendChild(newElement);
-    }
-
-    for ( var i = 0; i < response.results.length; i++ ) {
-      var createList = document.createElement('li');
-      createList.className = 'film';
-      displayFilmList.appendChild(createList);
-
-      makeList('h2', 'title', response.results[i].title, createList);
-
-
-    }
-
-  }
-
-  function createRequest(url, func) {
+  function createPersonRequest(url, id, propertyName) {
     var oReq = new XMLHttpRequest();
-    oReq.addEventListener('load', func);
+    oReq.addEventListener('load', getPerson);
     oReq.open('GET', url);
     oReq.send();
-  }
+
+    function getPerson() {
+      var response = JSON.parse(this.responseText);
+      console.log(response);
+      if ( Array.isArray(response[propertyName]) ) {
+        for ( var i = 0; i < response[propertyName].length; i++ ) {
+          createPersonRequest(response[propertyName][i], id, propertyName);
+        }
+      } else if ( response[propertyName].indexOf('http://') !== -1 ) {
+        createPersonRequest(response[propertyName], id, 'name');
+      } else {
+        document.querySelector(id).innerHTML = response[propertyName];
+      }
+    }
+   }
+
+   createPersonRequest('http://swapi.co/api/people/4/', '#person4Name', 'name');
+   createPersonRequest('http://swapi.co/api/people/4/', '#person4HomeWorld', 'homeworld');
+   createPersonRequest('http://swapi.co/api/people/14/', '#person14Name', 'name');
+   createPersonRequest('http://swapi.co/api/species/1/', '#person14Species', 'name');
+
+  // function getElements(id, objVal) {
+  //   var getElement = document.getElementById(id);
+  //   getElement.innerHTML = objVal;
+  // }
+
+  // function createFilmList(func) {
+  //   var response = JSON.parse(this.responseText);
+  //   var displayFilmList = document.getElementById('filmList');
+  //   console.log('results..', response);
+
+  //   function makeListElements(type, className, content, appendTarget) {
+  //     var newElement = document.createElement(type);
+  //     newElement.className = className;
+  //     newElement.innerHTML = content;
+  //     appendTarget.appendChild(newElement);
+  //   }
+
+  //   for ( var i = 0; i < response.results.length; i++ ) {
+  //     var createListElm = document.createElement('li');
+  //     createListElm.className = 'film';
+  //     displayFilmList.appendChild(createListElm);
+
+  //     makeListElements('h2', 'filmTitle', response.results[i].title, createListElm);
+  //     makeListElements('h3', 'planetName',  response.results[i].name, createListElm);
+
+  //     for ( var k = 0; k < response.results[i].planets.length; k++ ){
+  //     makeListElements('li', 'planet', response.results[i].planets[k], createListElm);
+
+  //   }
 
 
-createRequest('http://swapi.co/api/people/4/', darthVader);
-createRequest('http://swapi.co/api/people/14/', hanSolo);
-createRequest('http://swapi.co/api/films/', getFilmList);
+  //   }
+
+  // }
+
+
 
 })();

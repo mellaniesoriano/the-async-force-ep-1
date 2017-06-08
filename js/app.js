186fdb1dbaa35981c3fbe4b629a0668656a99ee4
mellaniesoriano <msoriano808@gmail.com>
2017-06-07 23:46:37 -1000

    created film title list using for loop in a function

--- js/app.js
+++ js/app.js
@@ -22,6 +22,29 @@
     getElements('person14Species', response.species);
   }
 
+  function getFilmList(func) {
+    var response = JSON.parse(this.responseText);
+    var displayFilmList = document.getElementById('filmList');
+    console.log(response.results);
+
+    function makeList(type, className, content, appendTarget) {
+      var newElement = document.createElement(type);
+      newElement.className = className;
+      newElement.innerHTML = content;
+      appendTarget.appendChild(newElement);
+    }
+
+    for ( var i = 0; i < response.results.length; i++ ) {
+      var createList = document.createElement('li');
+      createList.className = 'film';
+      displayFilmList.appendChild(createList);
+
+      makeList('h2', 'title', response.results[i].title, createList);
+
+
+    }
+
+  }
 
   function createRequest(url, func) {
     var oReq = new XMLHttpRequest();
@@ -33,6 +56,6 @@
 
 createRequest('http://swapi.co/api/people/4/', darthVader);
 createRequest('http://swapi.co/api/people/14/', hanSolo);
-createRequest('http://swapi.co/api/films/', films);
+createRequest('http://swapi.co/api/films/', getFilmList);
 
 })();
